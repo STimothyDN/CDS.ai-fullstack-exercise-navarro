732699fc491888a192d7d9905bd6f9a323019fed
Timothy Navarro <stimothydn@gmail.com>
2023-09-12 14:21:31 -0400

    hooked up native postgresql db and pulling from that db for census data api call

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import pandas as pd
 import os
 
 # FastAPI application from example on their website
 # https://fastapi.tiangolo.com/#example
 app = FastAPI()
+
+# use pathing to get the path to the census.csv file
+# this may be different locally vs in docker!
+census_path = f"{os.path.join(os.path.dirname(__file__))}/census.csv"
 
 origins = [
     "http://localhost",
@@ -20,23 +27,48 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# set up postgresql database
+Base = declarative_base()
+
+DATABASE_URL = "postgresql://timothyrjs:Tarheel1@localhost/mydb"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+class CensusModel(Base):
+    __tablename__ = 'census_data'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    age = Column(Integer)
+    education_level = Column(String)
+    race = Column(String)
+    sex = Column(String)
+    over_50k = Column(Integer)
+    over_50k_text = Column(String)
+    count = Column(Integer)
+
+Base.metadata.create_all(bind=engine)
+
+# load CSV into database
+def load_csv_to_db():
+    data = pd.read_csv(census_path)
+    session = SessionLocal()
+    for index, row in data.iterrows():
+        entry = CensusModel(age = row['age'], education_level = row['education_level'], race = row['race'], sex = row['sex'], over_50k = row['over_50k'], over_50k_text = row['over_50k_text'], count = row['count'])
+        session.add(entry)
+    session.commit()
+    session.close()
+
+@app.get("/load-csv")
+def load_csv_endpoint():
+    load_csv_to_db()
+    return {"status": "Data loaded succesfully"}
+
 @app.get("/census")
 def read_item():
-    
-    # use pathing to get the path to the census.csv file
-    # this may be different locally vs in docker!
-    census_path = f"{os.path.join(os.path.dirname(__file__))}/census.csv"
-    
-    # read CSV file using pandas
-    # https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
-    census_csv = pd.read_csv(census_path)
-
-    # convert to dict so that we return json to the frontend
-    # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_dict.html
-    census_csv = census_csv.to_dict(orient="records")
-    
-    # return to frontend
-    return census_csv
+    session = SessionLocal()
+    census_data = session.query(CensusModel).all()
+    result = [{"age": item.age, "education_level": item.education_level, "race": item.race, "sex": item.sex, "over_50k": item.over_50k, "over_50k_text": item.over_50k_text, "count": item.count} for item in census_data]
+    session.close()
+    return result
 
 @app.get("/summary-stats")
 def get_summary_stats():
